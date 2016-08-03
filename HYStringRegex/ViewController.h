d5d3c35160c26c950fb53e0f00a5c92de2fcdf2e
恒阳 <guanliang.wgl@alibaba-inc.com>
2016-08-03 21:40:43 +0800

    init proj

---
+++ HYStringRegex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYStringRegex
+//
+//  Created by 恒阳 on 16/8/3.
+//  Copyright © 2016年 恒阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYStringRegex/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HYStringRegex
+//
+//  Created by 恒阳 on 16/8/3.
+//  Copyright © 2016年 恒阳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"字符串校验";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
